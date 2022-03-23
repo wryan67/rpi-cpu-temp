049c42a82c96a5edd17140b34fc6e448a430b418
wade ryan <wryan67@gmail.com>
2022-03-22 19:36:32 -0500

    Initial Commit

---
+++ RPi-CPU-Temp/RPi_CPU_TempApp.swift
@@ -0,0 +1,17 @@
+//
+//  RPi_CPU_TempApp.swift
+//  RPi-CPU-Temp
+//
+//  Created by wade ryan on 3/22/22.
+//
+
+import SwiftUI
+
+@main
+struct RPi_CPU_TempApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
