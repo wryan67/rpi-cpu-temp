dd79e54aa3d99c3c0063f225707c2e32dd97bb83
wryan67 <wryan67@gmail.com>
2022-03-28 18:36:31 -0500

    update guuids

--- RPi-CPU-Temp/TemperatureService.swift
+++ RPi-CPU-Temp/TemperatureService.swift
@@ -10,12 +10,12 @@ public struct RaspberryPi {
     public struct TemperatureService  {
         
         // ServiceConfigurable
-        public static let uuid = "00000001-710e-4a5b-8d75-3e5b444bc3cf"
+        public static let uuid = "00000001-9233-4a5b-8d75-3e5b444bc3cf"
         public static let name = "RPi"
         public static let tag  = "CPU Temp Sensor Tag"
         
-        public static let tempCharacteristicUUID = "00000002-710e-4a5b-8d75-3e5b444bc3cf"
-        public static let unitCharacteristicUUID = "00000003-710e-4a5b-8d75-3e5b444bc3cf"
+        public static let tempCharacteristicUUID = "00000002-9233-4a5b-8d75-3e5b444bc3cf"
+        public static let unitCharacteristicUUID = "00000003-9233-4a5b-8d75-3e5b444bc3cf"
        
         // data
         public var mode: String
